6aa4f6bb21ffac991b39d92783298d5eaba4dfb2
KevinGage <dudeman02379@gmail.com>
2022-12-30 10:40:19 -0500

    first working version sockets

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,37 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import io from "Socket.IO-client";
+let socket;
 
 export default function Home() {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const socketInitializer = async () => {
+      console.log("fetching socket connection");
+      await fetch("/api/socket");
+      socket = io();
+
+      socket.on("connect", () => {
+        console.log("connected");
+      });
+
+      socket.on("update-input", (msg) => {
+        console.log(`received: ${msg}`);
+        setInput(msg);
+      });
+    };
+
+    if (!socket) {
+      socketInitializer();
+    }
+  }, []);
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+    socket.emit("input-change", e.target.value);
+  };
+
   return (
     <>
       <Head>
@@ -9,7 +40,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <h1>Home</h1>
+        <input
+          placeholder="Type something"
+          value={input}
+          onChange={onChangeHandler}
+        />
+      </main>
     </>
   );
 }
